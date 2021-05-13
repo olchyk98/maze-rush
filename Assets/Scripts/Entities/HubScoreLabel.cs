3d3655360b888f1d779351974e76811ec1a91740
Oles Odynets <olchyk98@gmail.com>
2021-05-13 18:00:35 +0200

    feat(core): implement player lives logic

--- Assets/Scripts/Entities/HubScoreLabel.cs
+++ Assets/Scripts/Entities/HubScoreLabel.cs
@@ -1,16 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 namespace Entities
 {
     public class HubScoreLabel : MonoBehaviour
     {
-        private void Awake()
+        private void Start()
         {
             int coins = PlayerPrefs.GetInt("PlayerCoins", 0);
-            GetComponent<TextMeshPro>().text = $"{coins}¢";
+            int lives = PlayerPrefs.GetInt("PlayerLives", 0);
+
+            GetComponent<TextMeshPro>().text = $"{coins}¢ | {lives}♥️";
         }
     }
 }
---
+++ Assets/Scripts/Process/PassiveStatsHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Process
+{
+    public class PassiveStatsHandler : MonoBehaviour
+    {
+        private void Awake ()
+        {
+            int coins = PlayerPrefs.GetInt("PlayerCoins", 0);
+            int lives = PlayerPrefs.GetInt("PlayerLives", 0);
+
+            if(lives <= 0)
+            {
+                PlayerPrefs.SetInt("PlayerLives", 5);
+                PlayerPrefs.SetInt("PlayerCoins", 0);
+            }
+
+            if(coins <= 0)
+            {
+                PlayerPrefs.SetInt("PlayerCoins", 0);
+            }
+        }
+    }
+
+}
