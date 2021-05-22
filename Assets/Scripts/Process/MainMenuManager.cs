22c99ac9e95636f2bdd4049f99179d0397761423
Oles Odynets <olchyk98@gmail.com>
2021-05-22 19:31:49 +0200

    feat(scenes): implement main menu

---
+++ Assets/Scripts/Process/MainMenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Process
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+
+        public void GoToHub()
+        {
+            SceneManager.LoadScene("HubScene");
+        }
+    }
+}
